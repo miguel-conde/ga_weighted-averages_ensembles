0cd90b9e5362c51c2862d5561b5c2affaa1f86e5
miguel-conde <miguelco2000@gmail.com>
2016-11-22 11:55:49 +0100

    Improved return in CV function

--- gaWARE.R
+++ gaWARE.R
@@ -63,7 +63,7 @@ cvOpt_gaWARE <- function(type = "cv", numFolds = 10, seed = 345,
     set.seed(seed)
     idxFolds <- createFolds(y, k = numFolds, returnTrain = TRUE)
     
-    ws <- lapply(idxFolds, function(fold_i) {
+    ga <- lapply(idxFolds, function(fold_i) {
       GA <- ga(type = "real-valued",
                fitness = function(x, ...) fitness_gaWARE(x, ...),
                ## '...' for fitness_gaWARE()
@@ -74,17 +74,31 @@ cvOpt_gaWARE <- function(type = "cv", numFolds = 10, seed = 345,
                maxiter = maxiter, names = c("w1", "w2", "w3"),
                seed = seed,
                monitor = monitor)
-      return(GA@solution)
+      return(GA)
     })
     
-    ws <- do.call(rbind, ws)
+    ws <- do.call(rbind, lapply(ga, function(x) return(x@solution)))
+    fv <- do.call(rbind, lapply(ga, function(x) return(x@fitnessValue)))
+    rownames(ws) <- rownames(fv)
+    colnames(fv) <- "Fitness function value"
     
-    # ws <- apply(ws, 2, mean, na.rm = TRUE)
+    ws_out           <- list()
     
-    ws_out <- list()
-    ws_out$mn <- apply(ws, 2, mean, na.rm = TRUE)
-    ws_out$sd <- apply(ws, 2, sd, na.rm = TRUE)
-    ws_out$t_test <- apply(ws, 2, t.test, na.rm = TRUE)
+    ws_out$GA        <- ga
+    
+    ws_out$ws        <- ws
+    ws_out$mn        <- apply(ws, 2, mean, na.rm = TRUE)
+    ws_out$sd        <- apply(ws, 2, sd, na.rm = TRUE)
+    ws_out$t_test    <- apply(ws, 2, t.test, na.rm = TRUE)
+    
+    if (maximize == FALSE) {
+      fv <- -fv 
+    }
+    
+    ws_out$fv        <- fv
+    ws_out$fv_mn     <- apply(fv, 2, mean, na.rm = TRUE)
+    ws_out$fv_sd     <- apply(fv, 2, sd, na.rm = TRUE)
+    ws_out$fv_t_test <- apply(fv, 2, t.test, na.rm = TRUE)
     
     return(ws_out)
   }
@@ -99,7 +113,7 @@ cvOpt_gaWARE <- function(type = "cv", numFolds = 10, seed = 345,
              maxiter = 100, names = c("w1", "w2", "w3"),
              seed = 789,
              monitor = NULL)
-    return(GA@solution)
+    return(GA)
   }
 }
 
